68c7bb2fbd9567e975a571519c07a47cf34739f1
Shaun McCance <shaunm@gnome.org>
2016-08-26 10:35:49 -0400

    Version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pintail',
-    version='0.3',
+    version='0.4',
     description='Build complete web site from Mallard sources.',
     packages=['pintail'],
     namespace_packages=['pintail'],
